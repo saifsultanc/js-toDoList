b625ff4ca7e89bab40238d6f1b1e8e2c706562f7
Saif Sultan <saifraikkonen@gmail.com>
2019-11-16 11:43:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,3 +33,31 @@ self.addEventListener('fetch', event => {
     );
     //console.log("Fetch intercepted for: ", event.request.url);
 });
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        // condition below is optional for analytics
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        }
+        
+        // to reset after the user choice has been processed
+        deferredPrompt = null;
+    }
+});
+  
+// for analytics
+window.addEventListener('appinstalled', (evt) => {
+    app.logEvent('a2hs', 'installed');
+});
